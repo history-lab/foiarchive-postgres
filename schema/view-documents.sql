99b4ddea8708801bf742070c1bd4ed75c07b2928
Ben Lis <ben@leismart.com>
2021-01-04 10:45:43 -0500

    Added topic_ids column to documents.

--- schema/view-documents.sql
+++ schema/view-documents.sql
@@ -10,13 +10,14 @@ with countries_list (doc_id, countries) as
             array_agg(p.person_id order by p.full_name)
          from persons p natural join persons_docs pd
          group by doc_id),
-     topics_list (doc_id, topics) as
+     topics_list (doc_id, topics, topic_ids) as
         (select td.doc_id,
-            array_agg(t.topic_name order by t.topic_name)
+            array_agg(t.topic_name order by t.topic_name),
+            array_agg(t.topic_id order by t.topic_name)
          from topics t natural join topics_docs td
          group by doc_id)
-select d.doc_id, corpus, classification, authored, title,
-    cl.countries, pl.persons, pl.person_ids, tl.topics,
+select d.doc_id, corpus, classification, authored, title, cl.countries,
+    pl.persons, pl.person_ids, tl.topics, tl.topic_ids,
     body, full_text
 from docs d left join countries_list cl on d.doc_id = cl.doc_id
             left join persons_list pl on d.doc_id = pl.doc_id
