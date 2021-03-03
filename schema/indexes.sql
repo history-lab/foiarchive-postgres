df524cc8dad99b13d65154e8ccebc55454fd376a
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-03-02 21:46:20 -0500

    Added docs index.

--- schema/indexes.sql
+++ schema/indexes.sql
@@ -1,4 +1,5 @@
 create index docs$authored on docs(authored);
+create index docs$corpus on docs(corpus);
 create index persons$full_name on persons(full_name);
 create index persons_docs$doc_id on persons_docs(doc_id);
 create index countries_docs$doc_id on countries_docs(doc_id);
