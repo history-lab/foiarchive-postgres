1915b39c7c608cfd77caf95e28098ffb6020f8cb
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-12-10 10:09:08 -0500

    Added inserts into classification; used the pre-calculated full text.

--- load/load-cia.sql
+++ load/load-cia.sql
@@ -1,9 +1,13 @@
-alter type foiarchive.classification_t add value 'restricted';
-alter type foiarchive.classification_t add value 'unknown';
+-- alter type foiarchive.classification_t add value 'restricted';
+-- alter type foiarchive.classification_t add value 'unknown';
 
--- insert into foiarchive.docs (doc_id, corpus, authored, title, body, source, 
---                             classification)
--- select doc_id, 'cia', authored, title, body, pdf, 
---       classification::classification_t 
--- from declassification_cia.docs_view
---where length(body) <= 1048575;
+insert into foiarchive.classifications  
+    (classification, sensitivity_level) values
+    ('restricted', 3),
+    ('unknown', 6);
+
+insert into foiarchive.docs (doc_id, corpus, authored, title, body, source, 
+                             classification, full_text)
+select doc_id, 'cia', authored, title, body, pdf, 
+       classification::classification_t, fts 
+from declassification_cia.docs_view;
