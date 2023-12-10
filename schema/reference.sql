6f64846125912d454c3f09114278a187e9917765
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-12-09 20:04:57 -0500

    Initial commit

---
+++ schema/reference.sql
@@ -0,0 +1,16 @@
+create table foiarchive.classifications
+    (classification     text        primary key,
+     sensitivity_level  smallint    not null);
+insert into foiarchive.classifications 
+    (classification, sensitivity_level) values
+    ('top secret', 1),
+    ('secret', 2),
+    ('confidential', 3),
+    ('limited official use', 4),
+    ('unclassified', 5);
+
+create table foiarchive.corpora
+    (corpus varchar(10) primary key);
+insert into foiarchive.corpora(corpus)
+    select distinct corpus
+        from foiarchive.docs;
