3f3cc1fd89cada67388868e575dce44a7cb8d52c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-12-09 19:48:37 -0500

    initial commit

---
+++ load/after-load.sql
@@ -0,0 +1,8 @@
+--
+-- absolutely critical step
+set maintenance_work_mem to '1 GB';
+create index on foiarchive.docs(authored);
+create index on foiarchive.docs(corpus);
+create index on foiarchive.docs using gin (full_text);
+create index on foiarchive.docs(classification)
+    where classification in ('top secret', 'secret', 'restricted');
