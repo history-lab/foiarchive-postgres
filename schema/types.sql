9f577f6d4eb12dbe8dd706aebbb2357d027d72ea
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-12-09 20:03:45 -0500

    Added values to corpus_t and classification_t

--- schema/types.sql
+++ schema/types.sql
@@ -1,5 +1,6 @@
 create domain id_d as varchar(40);
-create type corpus_t as enum('frus', 'cfpf', 'cia', 'clinton', 'pdb', 'kissinger');
+create type corpus_t as enum('frus', 'cfpf', 'clinton', 'pdb', 'kissinger',
+                             'nato', 'cia', 'un', 'worldbank');
 create type classification_t as
    enum('top secret','secret','confidential', 'unclassified',
-        'limited official use');
+        'limited official use', 'restricted', 'unknown');
