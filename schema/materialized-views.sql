58c44f27f11dffac6389c1c60ef1501822ee1645
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-06 08:50:00 -0500

    Added metrics; update column list on corpora

--- schema/materialized-views.sql
+++ schema/materialized-views.sql
@@ -1,5 +1,13 @@
+drop materialized view foiarchive.metrics
+create materialized view foiarchive.metrics as
+select count(doc_id) doc_cnt, sum(pg_cnt) pg_cnt, sum(word_cnt) word_cnt 
+   from foiarchive.docs;
+grant select on foiarchive.metrics to web_anon;   
+
 drop materialized view foiarchive.corpora;
 create materialized view foiarchive.corpora as
-select corpus, count(doc_id) doc_cnt
+select corpus, min(authored)::date begin_date, max(authored)::date end_date,  
+       count(doc_id) doc_cnt, sum(pg_cnt) pg_cnt, sum(word_cnt) word_cnt 
    from foiarchive.docs
    group by corpus;
+grant select on foiarchive.corpora to web_anon;
