553b8900c3f4cd9d82f5df437e151d14ce93924c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-05 20:58:10 -0500

    Added confidential to the classification index

--- load/after-load.sql
+++ load/after-load.sql
@@ -5,4 +5,6 @@ create index on foiarchive.docs(authored);
 create index on foiarchive.docs(corpus);
 create index on foiarchive.docs using gin (full_text);
 create index on foiarchive.docs(classification)
-    where classification in ('top secret', 'secret', 'restricted');
+    where classification in ('top secret', 'secret', 'confidential', 'restricted');
+-- recreate the foreign key constraints (at some point in the future)
+
