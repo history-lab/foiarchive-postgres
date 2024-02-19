c1f1ab13a5747d04fc6e6aea470fa410b08e4db2
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-19 09:32:56 -0500

    Fully qualified object references with foiarchive

--- schema/documents.sql
+++ schema/documents.sql
@@ -1,20 +1,23 @@
-create or replace view documents as
+create or replace view foiarchive.documents as
 with countries_list (doc_id, countries) as
         (select cd.doc_id,
             array_agg(c.country_name order by c.country_name) countries
-         from countries c natural join countries_docs cd
+         from foiarchive.countries c 
+            natural join foiarchive.countries_docs cd
          group by doc_id),
      persons_list (doc_id, persons, person_ids) as
         (select pd.doc_id,
             array_agg(p.full_name order by p.full_name),
             array_agg(p.person_id order by p.full_name)
-         from persons p natural join persons_docs pd
+         from foiarchive.persons p 
+            natural join foiarchive.persons_docs pd
          group by doc_id),
      topics_list (doc_id, topics, topic_ids) as
         (select td.doc_id,
             array_agg(t.topic_name order by t.topic_name),
             array_agg(t.topic_id order by t.topic_name)
-         from topics t natural join topics_docs td
+         from foiarchive.topics t 
+            natural join foiarchive.topic_docs td
          group by doc_id)
 select d.doc_id, corpus, classification, authored, title, cl.countries,
     pl.persons, pl.person_ids, tl.topics, tl.topic_ids,
