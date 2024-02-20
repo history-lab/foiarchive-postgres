bffee19c976063dea103f00ec972b60d08e6ff2b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-19 20:17:48 -0500

    Added column topic_cnt to foiarchive.corpora.topic_cnt

--- schema/materialized-views.sql
+++ schema/materialized-views.sql
@@ -6,8 +6,15 @@ grant select on foiarchive.totals to web_anon;
 
 drop materialized view foiarchive.corpora;
 create materialized view foiarchive.corpora as
-select corpus, min(authored)::date begin_date, max(authored)::date end_date,  
-       count(doc_id) doc_cnt, sum(pg_cnt) pg_cnt, sum(word_cnt) word_cnt 
-   from foiarchive.docs
-   group by corpus;
+with doc_stats(corpus, begin_date, end_date, doc_cnt, pg_cnt, word_cnt) as (
+   select corpus, min(authored)::date, max(authored)::date, 
+          count(doc_id), sum(pg_cnt), sum(word_cnt) 
+      from foiarchive.docs
+      group by corpus),
+topic_stats(corpus, topic_cnt) as (
+   select corpus, count(topic_id) topic_cnt
+      from foiarchive.topics
+      group by corpus)
+select ds.corpus, begin_date, end_date, doc_cnt, pg_cnt, word_cnt, topic_cnt 
+   from doc_stats ds left join topic_stats ts on ds.corpus = ts.corpus;
 grant select on foiarchive.corpora to web_anon;
