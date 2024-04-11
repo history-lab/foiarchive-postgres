1fa6e561ed1deed5bb750af1680adfeb614e0bf1
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-04-11 13:40:58 -0400

    Added refresh of materialized views

--- load/after-load.sql
+++ load/after-load.sql
@@ -7,4 +7,8 @@ create index on foiarchive.docs using gin (full_text);
 create index on foiarchive.docs(classification)
     where classification in ('top secret', 'secret', 'confidential', 'restricted');
 -- recreate the foreign key constraints (at some point in the future)
-
+-- refresh the materialized views
+REFRESH MATERIALIZED VIEW foiarchive.totals;
+REFRESH MATERIALIZED VIEW foiarchive.totals_decade;
+REFRESH MATERIALIZED VIEW foiarchive.corpora;
+REFRESH MATERIALIZED VIEW cnts_by_date;
