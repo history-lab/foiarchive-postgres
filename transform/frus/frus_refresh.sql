91bc9ac3ee83c962d7fd05a3d5312669eede7deb
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-04-11 13:43:41 -0400

    Added comments

--- transform/frus/frus_refresh.sql
+++ transform/frus/frus_refresh.sql
@@ -4,3 +4,30 @@ drop table foiarchive.docs_frus;
 \i docs_frus.sql
 drop table foiarchive.volumes;
 \i volumes.sql
+
+\i ../../load/before-load.sql  -- run manually
+-- 
+delete from foiarchive.docs where corpus = 'frus';
+insert into docs (doc_id, corpus, 
+                  classification, authored, 
+                  title, body,
+                  char_cnt, word_cnt, pg_cnt, 
+                  full_text)
+select fd.id, 'frus', 
+       lower(coalesce(fd.classification, 'unknown'))::classification_t, fd.date,
+       coalesce(fd.title, 'No Title - Document Id: ' || fd.id), fd.raw_body, 
+       length(fd.raw_body) char_cnt,
+       (select count(*) 
+          from ts_parse('default', fd.raw_body) t 
+          where t.tokid = 1) word_cnt, fd.num_pages pg_cnt,
+       to_tsvector('english', coalesce(fd.title, '') || ' ' || 
+                              coalesce(fd.body, '')  || ' ' ||
+                              coalesce(fd.chapt_title, '') || ' ' ||
+                              coalesce(fd.subject, '') || ' ' ||
+                              coalesce(fd.source, '') || ' ' ||
+                              'DOC ID: ' || fd.id || ' ' ||
+                              coalesce('SUBTYPE: ' || fd.subtype, '')) full_text
+    from declassification_frus.docs fd;
+
+\i ../../load/after-load.sql  -- run manually
+-- fix titles
