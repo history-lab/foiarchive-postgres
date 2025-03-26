7a735be97197a1fc60c36a64298d125b85384317
Ben Lis <bl2759@columbia.edu>
2025-03-26 08:57:43 -0400

    Updated to support new entities framework, make consistent with foiarchive-search, and improve performance.

--- schema/documents.sql
+++ schema/documents.sql
@@ -1,29 +1,27 @@
 create or replace view foiarchive.documents as
-with countries_list (doc_id, countries) as
-        (select cd.doc_id,
-            array_agg(c.country_name order by c.country_name) countries
-         from foiarchive.countries c 
-            natural join foiarchive.countries_docs cd
-         group by doc_id),
-     persons_list (doc_id, persons, person_ids) as
-        (select pd.doc_id,
-            array_agg(p.full_name order by p.full_name),
-            array_agg(p.person_id order by p.full_name)
-         from foiarchive.persons p 
-            natural join foiarchive.persons_docs pd
-         group by doc_id),
-     topics_list (doc_id, topics, topic_ids) as
-        (select td.doc_id,
-            array_agg(t.name order by t.name),
-            array_agg(t.topic_id order by t.name)
-         from foiarchive.topics t 
-            natural join foiarchive.topic_docs td
-         group by doc_id)
-select d.doc_id, corpus, classification, authored, title, cl.countries,
-    pl.persons, pl.person_ids, tl.topics, tl.topic_ids,
-    body, full_text
-from docs d left join countries_list cl on d.doc_id = cl.doc_id
-            left join persons_list pl on d.doc_id = pl.doc_id
-            left join topics_list tl on d.doc_id = tl.doc_id;
+select d.doc_id, d.corpus, classification, authored, d.title,
+       body, full_text, 
+       ta.topic_names, ta.topic_titles, ta.topic_scores, ta.topic_ids,
+       ea.entities, ea.entgroups, ea.wikidata_ids, ea.entity_ids
+from foiarchive.docs d left join lateral (
+     select array_agg(t.name order by td.score desc) topic_names,
+            array_agg(t.title order by td.score desc) topic_titles,
+            array_agg(td.score order by td.score desc) topic_scores,
+            array_agg(t.topic_id order by td.score desc) topic_ids
+         from foiarchive.topics t join foiarchive.topic_docs td 
+                                    on t.topic_id = td.topic_id and 
+                                       t.corpus = td.corpus
+         where td.doc_id = d.doc_id and td.corpus = d.corpus
+         group by td.doc_id) as ta on TRUE
+                      left join lateral (
+     select array_agg(e.entity order by e.entity) entities,
+            array_agg(e.entgroup order by e.entity) entgroups,
+            array_agg(e.wikidata_id order by e.entity) wikidata_ids,
+            array_agg(e.entity_id order by e.entity) entity_ids
+         from foiarchive.entities e join foiarchive.entity_docs ed 
+                                       on e.entity_id = ed.entity_id
+         where ed.doc_id = d.doc_id) as ea on TRUE;
 grant select on foiarchive.documents to web_anon;
-comment on view foiarchive.documents is 'Docs view with countries, persons, and topics lists. Used by the R API';
+comment on view foiarchive.documents is 'Docs view with entities and topics lists. Used by the R API';
+
+create index on foiarchive.entity_docs(doc_id); -- performance reasons
